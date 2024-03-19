44471b9c7f554fcf3cd3fd1886f4923ec399724a
Aryanandatv <aryanandatv146@gmail.com>
2024-03-19 11:17:56 +0530

    redux-counter

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import CounterMain from './Components/CounterMain'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     {/* <h1>Counter App</h1> */}
+     <CounterMain/>
+    </>
+  )
+}
+
+export default App
